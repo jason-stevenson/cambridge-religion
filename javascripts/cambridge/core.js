c0ac6c8ede36f38f76257b7ee978dd852272a57f
Jason Stevenson <jason@artofmultimedia.com.au>
2014-07-09 20:53:00 +1000

    first commit

---
+++ javascripts/cambridge/core.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+
+	// EOC
+ 	// ================================================================
+	$('.eoc').wrapInner('<div class="content"></div>');
+
+
+	// GLOBAL
+	// ================================================================
+	// Lighbox / Modal
+	$('<span class="icon-search"></span>').insertAfter('figure a[data-reveal-id] img');
+
+});
